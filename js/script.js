7e6c49c62d929cb04cecdb7e1bfbebe6c68d86cf
MikhailBagaev <mik-bagaev@yandex.ua>
2016-09-23 21:00:23 +0300

    last changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 'use strict';
-$(function(){	
+
+$(function(){
+$(document).ready(function () {	
 	var html = $('#parsed-resig').html();
     var mainObj = [    
         {
         headerTest: 'Проверь свои знания JavaScript'
-        },        
+        },               
         {
         	question: 'JavaScript это',
         	blockAnswer: ['Объектно-ориентированный скриптовый язык программирования',
         	              'Скриптовой язык программирования',
         	              'Прототипно-ориентированный скриптовый язык программирования',
         	              'Все выше перечисленое верно'],
-        	rightAnswer: 0
+        	rightAnswer: 2
         },
         { 
             question: 'Где в документе может располагаться тэг script по стандарту HTML?',
@@ -30,11 +32,9 @@ $(function(){
             rightAnswer: 3             
         },
         {
-            button: '<button id="calculate">Проверить результат</button>'
+            button: '<div class="calculate">Проверить результат</div>'
         }
-    ];  
-
-    
+    ];      
     
     localStorage.setItem('mainObj', JSON.stringify(mainObj));    
     mainObj = localStorage.getItem('mainObj');
@@ -44,21 +44,62 @@ $(function(){
     var content = tmpl(html, {
         data: mainObj}
         );
-
-    $body.append(content);     
     
-    var takeInputs = function (){
-        var inputs = document.querySelectorAll('.cbGroup1');
-        for (var i = 0; i < inputs.length; i++) {            
-        inputs[i].onchange = function() {
-            for (var i = 0; i < inputs.length; i++) {
-                inputs[i].checked = false;
+    $body.append(content);   
+
+    function checkInputs(){ 
+    var inputs = document.querySelectorAll('.' + myString[i]);    
+        for (var j = 0; j < inputs.length; j++) {
+        inputs[j].onchange = function() {
+            for (var k = 0; k < inputs.length; k++) {
+                inputs[k].checked = false;
                 this.checked = true;
                 }
             }
-        };    
+        }            
+    };   
+     
+    var myString = ['cbGroup1', 'cbGroup2', 'cbGroup3'];
+    for (var i = 0; i < myString.length; i++) {
+        checkInputs();        
     };
-    takeInputs();  
+
+    var score = 0;
+    var $checkedAnswer;
+    var $userAnswer = [];  
+    var $button = $('.calculate'); 
+    var allInputs = $('input');  
+    $($button).one('click', function(){       
+        $( ':checkbox:checked' ).each(function(){
+            $checkedAnswer = (this.value);
+            $userAnswer.push($checkedAnswer);
+            $('input[type="checkbox"]').attr('disabled', "disabled");
+        });
+        countScore(); 
+        showModal();     
+    }); 
+
+    function countScore(){     
+    for (var i = 0; i < mainObj.length - 2; i++) {        
+            if ($userAnswer[i] == mainObj[i + 1].rightAnswer) {
+                score +=10;
+            }
+    };     
+    return score;
+    }; 
+
+    function showModal(){
+        var $body = $('body');
+        var $modal = $('<div class="modal"><p class="modal-text">Вы набрали: ' + score + ' баллов</p></div>');
+        var $closeButton = $('<div class="close-button">ЗАКРЫТЬ</div>')
+        $body.append($modal);
+        $modal.append($closeButton);
+
+    $($closeButton).on('click', function() {        
+        $modal.remove();     
+    });
+    };    
+ });
 });
 
 
