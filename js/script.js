0209b3e689f6f73d006ef6a0b89ee874cd180bef
MikhailBagaev <mik-bagaev@yandex.ua>
2016-09-25 17:40:27 +0300

    possibility to try test again after closing modal window added

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 'use strict';
 
 $(function(){
-$(document).ready(function () {	
-	var html = $('#parsed-resig').html();
+$(document).ready(function () { 
+    var html = $('#parsed-resig').html();
     var mainObj = [    
         {
         headerTest: 'Проверь свои знания JavaScript'
         },               
         {
-        	question: 'JavaScript это',
-        	blockAnswer: ['Объектно-ориентированный скриптовый язык программирования',
-        	              'Скриптовой язык программирования',
-        	              'Прототипно-ориентированный скриптовый язык программирования',
-        	              'Все выше перечисленое верно'],
-        	rightAnswer: 2
+            question: 'JavaScript это',
+            blockAnswer: ['Объектно-ориентированный скриптовый язык программирования',
+                          'Скриптовой язык программирования',
+                          'Прототипно-ориентированный скриптовый язык программирования',
+                          'Все выше перечисленое верно'],
+            rightAnswer: 2
         },
         { 
             question: 'Где в документе может располагаться тэг script по стандарту HTML?',
@@ -32,7 +32,7 @@ $(document).ready(function () {
             rightAnswer: 3             
         },
         {
-            button: '<div class="calculate">Проверить результат</div>'
+            button: '<button class="calculate">Проверить результат</button>'
         }
     ];      
     
@@ -52,7 +52,7 @@ $(document).ready(function () {
         for (var j = 0; j < inputs.length; j++) {
         inputs[j].onchange = function() {
             for (var k = 0; k < inputs.length; k++) {
-                inputs[k].checked = false;
+                inputs[k].checked = false;                
                 this.checked = true;
                 }
             }
@@ -68,38 +68,60 @@ $(document).ready(function () {
     var $checkedAnswer;
     var $userAnswer = [];  
     var $button = $('.calculate'); 
-    var allInputs = $('input');  
-    $($button).one('click', function(){       
+    var allInputs = $('input');    
+    $($button).on('click', function(e){
+        e.preventDefault();
         $( ':checkbox:checked' ).each(function(){
             $checkedAnswer = (this.value);
-            $userAnswer.push($checkedAnswer);
-            $('input[type="checkbox"]').attr('disabled', "disabled");
-        });
+            $userAnswer.push($checkedAnswer);                
+        });        
         countScore(); 
-        showModal();     
+        showModal(); 
+        console.log(score);
+        console.log($userAnswer);              
     }); 
 
     function countScore(){     
     for (var i = 0; i < mainObj.length - 2; i++) {        
             if ($userAnswer[i] == mainObj[i + 1].rightAnswer) {
-                score +=10;
+                score += 10;
             }
     };     
-    return score;
+    // return score;
     }; 
+
+    function clearAll() {
+        $userAnswer = [];
+        score = 0;
+        $('input[type="checkbox"]').removeAttr('disabled', "disabled");    
+        $('input').prop('checked', false);
+    }
 
     function showModal(){
         var $body = $('body');
-        var $modal = $('<div class="modal"><p class="modal-text">Вы набрали: ' + score + ' баллов</p></div>');
+        var $modal = $('<div class="modal"><p class="modal-text">Вы успешно прошли тест!</p></div>');
+        var $modal2 = $('<div class="modal"><p class="modal-text">Вы не прошли тест! Вы можете попробовать еще раз.</p></div>');
         var $closeButton = $('<div class="close-button">ЗАКРЫТЬ</div>')
-        $body.append($modal);
-        $modal.append($closeButton);
+        if (score == (mainObj.length - 2) * 10) {
+            $body.append($modal);
+            $modal.append($closeButton);            
+        } else {
+            $body.append($modal2);
+            $modal2.append($closeButton);
+        }
+        $('input[type="checkbox"]').attr('disabled', "disabled");
+
 
     $($closeButton).on('click', function() {        
-        $modal.remove();     
+        $modal.remove();        
+        clearAll();                           
     });
-    };    
- });
+    $($closeButton).on('click', function() {        
+        $modal2.remove();        
+        clearAll()                      
+    });    
+ }; 
+});
 });
 
 
@@ -115,16 +137,17 @@ $(document).ready(function () {
 
 
 
- 
-
-
-
 
  
 
 
 
-	
+
+ 
+
+
+
+    
 
 
 
